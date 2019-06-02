c83cd6602f5f723506aeb46b46cca4608269a2e0
fmw <fmwph@163.com>
2019-06-02 18:49:19 +0800

    1

---
+++ springboot-properties/src/main/java/org/spring/springboot/property/XMLObj.java
@@ -0,0 +1,31 @@
+package org.spring.springboot.property;
+
+/**
+ * Created by Administrator on 2019/1/29.
+ */
+public class XMLObj {
+	private String name;
+
+
+	public XMLObj() {
+
+	}
+
+
+	public XMLObj(String name) {
+
+		this.name = name;
+	}
+
+
+	public String getName() {
+
+		return name;
+	}
+
+
+	public void setName(String name) {
+
+		this.name = name;
+	}
+}
